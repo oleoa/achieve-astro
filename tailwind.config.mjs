bb788fb72932acf66e3f569863dc90d2a8c864a7
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-12-03 18:02:26 +0000

    Readme file update

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,14 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+  		colors: {
+          blue: '#1e40af',
+        },
+        spacing: {
+          'margin-x': '7rem',
+        }
+		},
 	},
 	plugins: [],
 }
