63578cefcfad6ce646537334c5114b255d5fdb66
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2025-01-23 09:22:04 +0000

    Add about us page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-  		colors: {
-          blue: '#1e40af',
-        },
-        spacing: {
-          'margin-x': '7rem',
-        }
-		},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#1e40af",
+      },
+      spacing: {
+        "margin-x": "7rem",
+      },
+    },
+  },
+  plugins: [],
+};
