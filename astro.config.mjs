3f6fc6d909b9f3657ad86cc5c2cc1a390293ba60
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2026-01-09 12:33:22 +0000

    Add auto stripe payment link

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
 // @ts-check
 import { defineConfig } from "astro/config";
-
+import netlify from "@astrojs/netlify";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server", // keep server output if you need SSR / server routes
   integrations: [tailwind()],
   redirects: {
     "/": "/en",
   },
+  adapter: netlify(),
 });
